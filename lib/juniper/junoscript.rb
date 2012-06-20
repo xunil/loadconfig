2c9f0d4bca00e562738a83ac7df815d08f3655c1
Robert Liesenfeld <xunil@tumblr.com>
2012-06-20 09:26:36 -0700

    Initial import.

---
+++ lib/juniper/junoscript.rb
@@ -0,0 +1,70 @@
+require 'rubygems'
+require 'net/ssh'
+require 'rexml/document'
+
+include REXML
+
+module Juniper
+  class JUNOScript
+    def initialize(hostname, username, options = {})
+      ssh_opts = {}
+      [:keys, :logger, :password].each do |key|
+        ssh_opts[key] = options[key] if options.has_key?(key)
+      end
+      @session = Net::SSH.start(hostname, username, options = ssh_opts)
+      @logger = @session.logger
+    end
+
+    def rpc_request(xml)
+      exec(junoscript(rpc(xml)))
+    end
+
+    def exec(xml)
+      buffer = Net::SSH::Buffer.new
+      @session.open_channel do |channel|
+        channel.exec("junoscript") do |ch, success|
+          if success
+            if block_given?
+              yield channel
+            else
+              # Construct XML document
+              doc = Document.new(xml.to_s)
+              doc << XMLDecl.new
+
+              # Send to JunOS device
+              @logger.debug "sending data: #{doc.to_s}"
+              channel.send_data(doc.to_s)
+              channel.on_data {|ch, data| buffer.append(data)}
+              channel.on_extended_data {|ch, type, data| @logger.warn "Extended data (type #{type}) received: #{data}"}
+            end
+          else
+            channel.close
+            return nil
+          end
+        end
+      end
+      @session.loop
+      buffer.read
+    end
+
+    def junoscript(elements)
+      js = Element.new('junoscript')
+      js.attributes['version'] = '1.0'
+      # REXML::Element overrides #to_a, this is the only way to support passing of
+      # single elements or arrays of elements
+      [elements].flatten.each do |element|
+        js << ( element.class == REXML::Element ? element : Document.new(element).root )
+      end
+      @logger.debug "in junoscript: js=#{js.to_s}"
+      js
+    end
+
+    def rpc(xml)
+      rpc = Element.new('rpc')
+      rpc << ( xml.class == REXML::Element ? xml : Document.new(xml).root )
+      rpc
+    end
+  end
+end
+
+
---
+++ loadconfig.rb
@@ -0,0 +1,120 @@
+#!/usr/bin/ruby
+require 'optparse'
+require 'rubygems'
+require 'rexml/document'
+require 'erubis'
+# Ugly hack required for net/ssh
+ENV['HOME'] = '/'
+require File.join(File.dirname(__FILE__), "lib", "juniper", "junoscript")
+
+module Juniper
+  class ConfigLoader
+    def initialize(options)
+      @ssh_opts = {}
+      @ssh_opts[:keys] = [options[:ssh_key]] unless options[:ssh_key].nil?
+      @ssh_opts[:password] = options[:password] unless options[:password].nil?
+      @dryrun = options[:dryrun]
+      @debug = options[:debug]
+      @js = Juniper::JUNOScript.new(options[:hostname], options[:username], options = @ssh_opts)
+    end
+
+    def load(filename)
+      config_text = File.open(filename).read()
+      eruby = Erubis::Eruby.new(config_text)
+
+      reply = @js.rpc_request('<get-configuration/>')
+      current_config = Document.new(reply)
+      context = {:current_config => current_config}
+
+      config_changes = Element.new('load-configuration')
+      config_changes.attributes['format'] = 'text'
+      config_changes.attributes['action'] = 'merge'
+      config_changes.add_element('configuration-text').text = eruby.evaluate(context)
+
+      success, request, reply = commit_config(config_changes, check=@dryrun)
+      puts "DEBUG: Request: #{request}" if @debug
+      puts "DEBUG: Reply: #{reply}" if @debug
+      return success
+    end
+
+    def commit_config(config_changes, check)
+      private_config = Element.new('open-configuration')
+      private_config.add_element('private')
+      commit_tag = Element.new('commit-configuration')
+      if check
+        commit_tag.add_element('check')
+      else
+        log = Element.new('log')
+        log.text = "Committed by loadconfig.rb"
+        commit_tag << log
+      end
+      elements = [@js.rpc(private_config), @js.rpc(config_changes), @js.rpc(commit_tag)]
+      request = @js.junoscript(elements)
+
+      reply = @js.exec(request)
+      reply_doc = Document.new(reply)
+      success_xpath = (check ? '//commit-check-success' : '//commit-success')
+      if (!XPath.match(reply_doc, "//xnm:error").empty?) or (XPath.match(reply_doc, success_xpath).empty?)
+        return false, request.to_s, reply
+      end
+      return true, request.to_s, reply
+    end
+  end
+end
+options = {:dryrun => false, :debug => false}
+
+optparse = OptionParser.new do |opts|
+  opts.on('-u', '--username=USERNAME', 'Juniper username') do |username|
+    options[:username] = username
+  end
+
+  opts.on('-p', '--password=PASSWORD', 'Juniper password') do |password|
+    options[:password] = password
+  end
+
+  opts.on('-k', '--ssh-key=KEYFILE', 'Juniper SSH key filename') do |ssh_key|
+    options[:ssh_key] = ssh_key
+  end
+
+  opts.on('-H', '--hostname=HOSTNAME', 'Hostname of Juniper device') do |hostname|
+    options[:hostname] = hostname
+  end
+
+  opts.on('-f', '--filename=FILENAME', 'Filename of configuration snippet to load') do |filename|
+    options[:filename] = filename
+  end
+
+  opts.on('-n', '--dryrun', 'Verify configuration commit would succeed but do not commit') do
+    options[:dryrun] = true
+  end
+
+  opts.on('-d', '--debug', 'Output JunOScript interactions') do
+    options[:debug] = true
+  end
+
+  opts.on('-h', '--help', 'Display usage message') do
+    puts opts
+    exit
+  end
+end
+
+begin
+  optparse.parse!
+  mandatory = [:username, :hostname, :filename]
+  missing = mandatory.select {|param| options[param].nil?}
+  if not missing.empty?
+    puts "Missing required options: #{missing.join(', ')}"
+    puts optparse
+    exit
+  end
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+  puts $!.to_s
+  puts optparse
+  exit
+end
+
+loader = Juniper::ConfigLoader.new(options)
+success = loader.load(options[:filename])
+
+puts "%s %s%s" % [options[:hostname], (options[:dryrun] ? "check " : ""), (success ? "success" : "fail")]
+exit((success ? 0 : 1))
